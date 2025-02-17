0b0d0e1f0986adbc751b568c4f5fb3877b6a6e9a
Sviatoslav Kalinichuk <99319855+1kalina@users.noreply.github.com>
2025-02-17 19:10:00 +0100

    Update Exercise.java

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,7 +1,17 @@
 //package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+    public static String Calculate(int number) {
+        // Робимо перевірку створюючи новий клас при не виконаній умові
+        // Новий клас потрібний для try-catch (Мабуть не ефективний спосіб?)
+        if (number < 100000 || number > 999999) {
+            throw new IllegalArgumentException("Число має бути шестизначним (100000-999999)");
+        }
+        String binary = Integer.toBinaryString(number);
+        String octal = Integer.toOctalString(number);
+        String hex = Integer.toHexString(number);
+        // Збираємо усі дані в один рядок щоб не створювати 3 змінних
+        return String.join(", ", binary, octal, hex);
     }
 }
